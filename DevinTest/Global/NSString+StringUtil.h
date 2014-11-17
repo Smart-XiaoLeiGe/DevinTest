d10d4c152a8d3b13af2087b971a279c0e9ecec1a
Devin Wang <wanglei_sh163@163.com>
2014-11-17 20:03:16 +0800

    add string util

---
+++ DevinTest/Global/NSString+StringUtil.h
@@ -0,0 +1,13 @@
+//
+//  NSString+StringUtil.h
+//  DevinTest
+//
+//  Created by Devin on 11/17/14.
+//  Copyright (c) 2014 Devin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (StringUtil)
+
+@end
---
+++ DevinTest/Global/NSString+StringUtil.m
@@ -0,0 +1,13 @@
+//
+//  NSString+StringUtil.m
+//  DevinTest
+//
+//  Created by Devin on 11/17/14.
+//  Copyright (c) 2014 Devin. All rights reserved.
+//
+
+#import "NSString+StringUtil.h"
+
+@implementation NSString (StringUtil)
+
+@end
